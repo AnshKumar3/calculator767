600f44bebdff78934d550683f4bca628ffa00f87
Ansh Kumar <88948265+AnshKumar3@users.noreply.github.com>
2024-08-12 16:07:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,179 +1,232 @@
-import { useReducer } from "react"
+import { useReducer, useState } from "react"
 import DigitButton from "./DigitButton"
 import OperationButton from "./OperationButton"
 import "./styles.css"
 
-export const ACTIONS = {
-  ADD_DIGIT: "add-digit",
-  CHOOSE_OPERATION: "choose-operation",
-  CLEAR: "clear",
-  DELETE_DIGIT: "delete-digit",
-  EVALUATE: "evaluate",
-}
-
-function reducer(state, { type, payload }) {
-  switch (type) {
-    case ACTIONS.ADD_DIGIT:
-      if (state.overwrite) {
-        return {
-          ...state,
-          currentOperand: payload.digit,
-          overwrite: false,
-        }
-      }
-      if (payload.digit === "0" && state.currentOperand === "0") {
-        return state
-      }
-      if (payload.digit === "." && state.currentOperand.includes(".")) {
-        return state
-      }
-
-      return {
-        ...state,
-        currentOperand: `${state.currentOperand || ""}${payload.digit}`,
-      }
-    case ACTIONS.CHOOSE_OPERATION:
-      if (state.currentOperand == null && state.previousOperand == null) {
-        return state
-      }
-
-      if (state.currentOperand == null) {
-        return {
-          ...state,
-          operation: payload.operation,
-        }
-      }
-
-      if (state.previousOperand == null) {
-        return {
-          ...state,
-          operation: payload.operation,
-          previousOperand: state.currentOperand,
-          currentOperand: null,
-        }
-      }
-
-      return {
-        ...state,
-        previousOperand: evaluate(state),
-        operation: payload.operation,
-        currentOperand: null,
-      }
-    case ACTIONS.CLEAR:
-      return {}
-    case ACTIONS.DELETE_DIGIT:
-      if (state.overwrite) {
-        return {
-          ...state,
-          overwrite: false,
-          currentOperand: null,
-        }
-      }
-      if (state.currentOperand == null) return state
-      if (state.currentOperand.length === 1) {
-        return { ...state, currentOperand: null }
-      }
-
-      return {
-        ...state,
-        currentOperand: state.currentOperand.slice(0, -1),
-      }
-    case ACTIONS.EVALUATE:
-      if (
-        state.operation == null ||
-        state.currentOperand == null ||
-        state.previousOperand == null
-      ) {
-        return state
-      }
-
-      return {
-        ...state,
-        overwrite: true,
-        previousOperand: null,
-        operation: null,
-        currentOperand: evaluate(state),
-      }
-  }
-}
-
-function evaluate({ currentOperand, previousOperand, operation }) {
-  const prev = parseFloat(previousOperand)
-  const current = parseFloat(currentOperand)
-  if (isNaN(prev) || isNaN(current)) return ""
-  let computation = ""
-  switch (operation) {
-    case "+":
-      computation = prev + current
-      break
-    case "-":
-      computation = prev - current
-      break
-    case "*":
-      computation = prev * current
-      break
-    case "÷":
-      computation = prev / current
-      break
-  }
-
-  return computation.toString()
-}
-
-const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
-  maximumFractionDigits: 0,
-})
-function formatOperand(operand) {
-  if (operand == null) return
-  const [integer, decimal] = operand.split(".")
-  if (decimal == null) return INTEGER_FORMATTER.format(integer)
-  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
-}
-
 function App() {
-  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
-    reducer,
-    {}
-  )
+
+const [answer,setAnswer]=useState("0");
+const[expression,setExpression]=useState("");
+const et = expression.trim();
+ 
+const isOperator = (symbol) => {
+  return /[*/+-]/.test(symbol);
+};
+
+const buttonPress = (symbol) => {
+  if (symbol === "clear") {
+    setAnswer("");
+    setExpression("0");
+  } else if (symbol === "negative") {
+    if (answer === "") return;
+    setAnswer(
+      answer.toString().charAt(0) === "-" ? answer.slice(1) : "-" + answer
+    );
+  } else if (symbol === "percent") {
+    if (answer === "") return;
+    setAnswer((parseFloat(answer) / 100).toString());
+  } else if (isOperator(symbol)) {
+    setExpression(et + " " + symbol + " ");
+  } else if (symbol === "=") {
+    calculate();
+  } else if (symbol === "0") {
+    if (expression.charAt(0) !== "0") {
+      setExpression(expression + symbol);
+    }
+  } else if (symbol === ".") {
+    // split by operators and get last number
+    const lastNumber = expression.split(/[-+/*]/g).pop();
+    if (!lastNumber) return;
+    console.log("lastNumber :>> ", lastNumber);
+    // if last number already has a decimal, don't add another
+    if (lastNumber?.includes(".")) return;
+    setExpression(expression + symbol);
+  } else {
+    if (expression.charAt(0) === "0") {
+      setExpression(expression.slice(1) + symbol);
+    } else {
+      setExpression(expression + symbol);
+    }
+  }
+};
+const calculate = () => {
+  // if last char is an operator, do nothing
+  if (isOperator(et.charAt(et.length - 1))) return;
+  // clean the expression so that two operators in a row uses the last operator
+  // 5 * - + 5 = 10
+  const parts = et.split(" ");
+  const newParts = [];
+
+  // go through parts backwards
+  for (let i = parts.length - 1; i >= 0; i--) {
+    if (["*", "/", "+"].includes(parts[i]) && isOperator(parts[i - 1])) {
+      newParts.unshift(parts[i]);
+      let j = 0;
+      let k = i - 1;
+      while (isOperator(parts[k])) {
+        k--;
+        j++;
+      }
+      i -= j;
+    } else {
+      newParts.unshift(parts[i]);
+    }
+  }
+  const newExpression = newParts.join(" ");
+  if (isOperator(newExpression.charAt(0))) {
+    setAnswer(eval(answer + newExpression) );
+  } else {
+    setAnswer(eval(newExpression) );
+  }
+  setExpression("");
+};
+
 
   return (
     <div className="calculator-grid">
-      <div className="output">
+      <div className="output" id="display">
         <div className="previous-operand">
-          {formatOperand(previousOperand)} {operation}
         </div>
-        <div className="current-operand">{formatOperand(currentOperand)}</div>
+
+        <div className="current-operand" id="answer">{answer}</div>
+        <div id="expression">{expression}</div>
+
       </div>
+      
       <button
-        className="span-two"
-        onClick={() => dispatch({ type: ACTIONS.CLEAR })}
-      >
-        AC
-      </button>
-      <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>
-        DEL
-      </button>
-      <OperationButton operation="÷" dispatch={dispatch} />
-      <DigitButton digit="1" dispatch={dispatch} />
-      <DigitButton digit="2" dispatch={dispatch} />
-      <DigitButton digit="3" dispatch={dispatch} />
-      <OperationButton operation="*" dispatch={dispatch} />
-      <DigitButton digit="4" dispatch={dispatch} />
-      <DigitButton digit="5" dispatch={dispatch} />
-      <DigitButton digit="6" dispatch={dispatch} />
-      <OperationButton operation="+" dispatch={dispatch} />
-      <DigitButton digit="7" dispatch={dispatch} />
-      <DigitButton digit="8" dispatch={dispatch} />
-      <DigitButton digit="9" dispatch={dispatch} />
-      <OperationButton operation="-" dispatch={dispatch} />
-      <DigitButton digit="." dispatch={dispatch} />
-      <DigitButton digit="0" dispatch={dispatch} />
-      <button
-        className="span-two"
-        onClick={() => dispatch({ type: ACTIONS.EVALUATE })}
-      >
-        =
-      </button>
+            id="clear"
+            onClick={() => buttonPress("clear")}
+            className="light-gray"
+          >
+            C
+          </button>
+          <button
+            id="negative"
+            onClick={() => buttonPress("negative")}
+            className="light-gray"
+          >
+            +/-
+          </button>
+          <button
+            id="percentage"
+            onClick={() => buttonPress("percentage")}
+            className="light-gray"
+          >
+            %
+          </button>
+          <button
+            id="divide"
+            onClick={() => buttonPress("/")}
+            className="yellow"
+          >
+            /
+          </button>
+          <button
+            id="seven"
+            onClick={() => buttonPress("7")}
+            className="dark-gray"
+          >
+            7
+          </button>
+          <button
+            id="eight"
+            onClick={() => buttonPress("8")}
+            className="dark-gray"
+          >
+            8
+          </button>
+          <button
+            id="nine"
+            onClick={() => buttonPress("9")}
+            className="dark-gray"
+          >
+            9
+          </button>
+          <button
+            id="multiply"
+            onClick={() => buttonPress("*")}
+            className="yellow"
+          >
+            *
+          </button>
+          <button
+            id="four"
+            onClick={() => buttonPress("4")}
+            className="dark-gray"
+          >
+            4
+          </button>
+          <button
+            id="five"
+            onClick={() => buttonPress("5")}
+            className="dark-gray"
+          >
+            5
+          </button>
+          <button
+            id="six"
+            onClick={() => buttonPress("6")}
+            className="dark-gray"
+          >
+            6
+          </button>
+          <button
+            id="subtract"
+            onClick={() => buttonPress("-")}
+            className="yellow"
+          >
+            -
+          </button>
+          <button
+            id="one"
+            onClick={() => buttonPress("1")}
+            className="dark-gray"
+          >
+            1
+          </button>
+          <button
+            id="two"
+            onClick={() => buttonPress("2")}
+            className="dark-gray"
+          >
+            2
+          </button>
+          <button
+            id="three"
+            onClick={() => buttonPress("3")}
+            className="dark-gray"
+          >
+            3
+          </button>
+          <button id="add" onClick={() => buttonPress("+")} className="yellow">
+            +
+          </button>
+          <button
+            id="zero"
+            onClick={() => buttonPress("0")}
+            className="dark-gray"
+          >
+            0
+          </button>
+          <button
+            id="decimal"
+            onClick={() => buttonPress(".")}
+            className="dark-gray"
+          >
+            .
+          </button>
+          <button
+            id="equals"
+            onClick={() => buttonPress("=")}
+            className="yellow"
+          >
+            =
+          </button>
+      
+      
+      <button className="span-two" id="divide">/</button>
+
+
     </div>
   )
 }
